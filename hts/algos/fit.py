b77f5641111312b334807f8353cb3067aa47bc8e
carlo.mazzafero <carlo.mazzaferro@circ.com>
2019-11-23 12:12:37 +0100

    cosmetic changes

--- hts/algos/fit.py
+++ hts/algos/fit.py
@@ -26,7 +26,6 @@ def fitForecast(y, h, sumMat, nodes, method, freq, include_history, cap, capF, c
                 yearly_seasonality, weekly_seasonality, daily_seasonality, holidays, seasonality_prior_scale, \
                 holidays_prior_scale, changepoint_prior_scale, mcmc_samples, interval_width, uncertainty_samples, \
                 boxcoxT, skipFitting):
-
     forecastsDict = {}
     mse = {}
     resids = {}
@@ -35,17 +34,17 @@ def fitForecast(y, h, sumMat, nodes, method, freq, include_history, cap, capF, c
     # If you have a ditionary of Prophet Dataframes already, skip the prophet part, and put all the values into a dictionary
     ##
     if skipFitting == True:
-        for key in range(len(y.columns.tolist())-1):
-            forecastsDict[key] = pd.DataFrame(y.iloc[:,key+1])
-            forecastsDict[key] = forecastsDict[key].rename(columns = {forecastsDict[key].columns[0] : 'yhat'})
+        for key in range(len(y.columns.tolist()) - 1):
+            forecastsDict[key] = pd.DataFrame(y.iloc[:, key + 1])
+            forecastsDict[key] = forecastsDict[key].rename(columns={forecastsDict[key].columns[0]: 'yhat'})
 
     if skipFitting == False:
 
         if method == 'FP':
-            nForecasts = sum(list(map(sum, nodes)))+1
+            nForecasts = sum(list(map(sum, nodes))) + 1
 
         for node in range(nForecasts):
-            nodeToForecast = pd.concat([y.iloc[:, [0]], y.iloc[:, node+1]], axis = 1)
+            nodeToForecast = pd.concat([y.iloc[:, [0]], y.iloc[:, node + 1]], axis=1)
             if isinstance(cap, pd.DataFrame):
                 cap1 = cap.iloc[:, node]
             else:
@@ -67,8 +66,8 @@ def fitForecast(y, h, sumMat, nodes, method, freq, include_history, cap, capF, c
             ##
             with contextlib.redirect_stdout(open(os.devnull, "w")):
                 # Prophet related stuff
-                nodeToForecast = nodeToForecast.rename(columns = {nodeToForecast.columns[0] : 'ds'})
-                nodeToForecast = nodeToForecast.rename(columns = {nodeToForecast.columns[1] : 'y'})
+                nodeToForecast = nodeToForecast.rename(columns={nodeToForecast.columns[0]: 'ds'})
+                nodeToForecast = nodeToForecast.rename(columns={nodeToForecast.columns[1]: 'y'})
                 if capF is None:
                     growth = 'linear'
                     m = Prophet(growth,
@@ -101,15 +100,15 @@ def fitForecast(y, h, sumMat, nodes, method, freq, include_history, cap, capF, c
                                 uncertainty_samples=uncertainty_samples)
                     nodeToForecast['cap'] = cap1
                 m.fit(nodeToForecast)
-                future = m.make_future_dataframe(periods = h, freq = freq, include_history = include_history)
+                future = m.make_future_dataframe(periods=h, freq=freq, include_history=include_history)
                 if capF is not None:
                     future['cap'] = cap2
                 ##
                 # Base Forecasts, Residuals, and MSE
                 ##
                 forecastsDict[node] = m.predict(future)
-                resids[node] = y.iloc[:, node+1] - forecastsDict[node].yhat[:-h].values
-                mse[node] = np.mean(np.array(resids[node])**2)
+                resids[node] = y.iloc[:, node + 1] - forecastsDict[node].yhat[:-h].values
+                mse[node] = np.mean(np.array(resids[node]) ** 2)
                 ##
                 # If logistic use exponential function, so that values can be added correctly
                 ##
@@ -133,7 +132,7 @@ def fitForecast(y, h, sumMat, nodes, method, freq, include_history, cap, capF, c
     ##
     if method == 'BU' or method == 'AHP' or method == 'PHA':
         y1 = y.copy()
-        nCols = len(list(forecastsDict.keys()))+1
+        nCols = len(list(forecastsDict.keys())) + 1
         if method == 'BU':
             '''
              Pros:
@@ -141,14 +140,14 @@ def fitForecast(y, h, sumMat, nodes, method, freq, include_history, cap, capF, c
              Cons:
                Bottom level data can be noisy and more challenging to model and forecast
             '''
-            hatMat = np.zeros([len(forecastsDict[0].yhat),1])
-            for key in range(nCols-sumMat.shape[1]-1, nCols-1):
+            hatMat = np.zeros([len(forecastsDict[0].yhat), 1])
+            for key in range(nCols - sumMat.shape[1] - 1, nCols - 1):
                 f1 = np.array(forecastsDict[key].yhat)
                 f2 = f1[:, np.newaxis]
                 if np.all(hatMat == 0):
                     hatMat = f2
                 else:
-                    hatMat = np.concatenate((hatMat, f2), axis = 1)
+                    hatMat = np.concatenate((hatMat, f2), axis=1)
 
         if method == 'AHP':
             '''
@@ -158,19 +157,19 @@ def fitForecast(y, h, sumMat, nodes, method, freq, include_history, cap, capF, c
                Unable to capture individual series dynamics
             '''
             if boxcoxT is not None:
-                for column in range(len(y.columns.tolist())-1):
-                    y1.iloc[:,column+1] = inv_boxcox(y1.iloc[:, column+1], boxcoxT[column])
+                for column in range(len(y.columns.tolist()) - 1):
+                    y1.iloc[:, column + 1] = inv_boxcox(y1.iloc[:, column + 1], boxcoxT[column])
             ##
             # Find Proportions
             ##
             fcst = forecastsDict[0].yhat
             fcst = fcst[:, np.newaxis]
             numBTS = sumMat.shape[1]
-            btsDat = pd.DataFrame(y1.iloc[:,nCols-numBTS:nCols])
-            divs = np.divide(np.transpose(np.array(btsDat)),np.array(y1.iloc[:,1]))
+            btsDat = pd.DataFrame(y1.iloc[:, nCols - numBTS:nCols])
+            divs = np.divide(np.transpose(np.array(btsDat)), np.array(y1.iloc[:, 1]))
             props = divs.mean(1)
             props = props[:, np.newaxis]
-            hatMat = np.dot(np.array(fcst),np.transpose(props))
+            hatMat = np.dot(np.array(fcst), np.transpose(props))
 
         if method == 'PHA':
             '''
@@ -180,35 +179,36 @@ def fitForecast(y, h, sumMat, nodes, method, freq, include_history, cap, capF, c
                Unable to capture individual series dynamics
             '''
             if boxcoxT is not None:
-                for column in range(len(y.columns.tolist())-1):
-                    y1.iloc[:,column+1] = inv_boxcox(y1.iloc[:, column+1], boxcoxT[column])
+                for column in range(len(y.columns.tolist()) - 1):
+                    y1.iloc[:, column + 1] = inv_boxcox(y1.iloc[:, column + 1], boxcoxT[column])
             ##
             # Find Proportions
             ##
             fcst = forecastsDict[0].yhat
             fcst = fcst[:, np.newaxis]
             numBTS = sumMat.shape[1]
-            btsDat = pd.DataFrame(y1.iloc[:,nCols-numBTS:nCols])
+            btsDat = pd.DataFrame(y1.iloc[:, nCols - numBTS:nCols])
             btsSum = btsDat.sum(0)
-            topSum = sum(y1.iloc[:,1])
-            props = btsSum/topSum
+            topSum = sum(y1.iloc[:, 1])
+            props = btsSum / topSum
             props = props[:, np.newaxis]
-            hatMat = np.dot(np.array(fcst),np.transpose(props))
+            hatMat = np.dot(np.array(fcst), np.transpose(props))
 
-        newMat = np.empty([hatMat.shape[0],sumMat.shape[0]])
+        newMat = np.empty([hatMat.shape[0], sumMat.shape[0]])
         for i in range(hatMat.shape[0]):
-            newMat[i,:] = np.dot(sumMat, np.transpose(hatMat[i,:]))
+            newMat[i, :] = np.dot(sumMat, np.transpose(hatMat[i, :]))
 
     if method == 'FP':
         newMat = forecastProp(forecastsDict, nodes)
     if method == 'OLS' or method == 'WLSS' or method == 'WLSV':
         if capF is not None:
-            print("An error might occur because of how these methods are defined (They can produce negative values). If it does, then please use another method")
+            print(
+                "An error might occur because of how these methods are defined (They can produce negative values). If it does, then please use another method")
         newMat = optimalComb(forecastsDict, sumMat, method, mse)
 
     for key in forecastsDict.keys():
         values = forecastsDict[key].yhat.values
-        values = newMat[:,key]
+        values = newMat[:, key]
         forecastsDict[key].yhat = values
         ##
         # If Logistic fit values with natural log function to revert back to format of input
@@ -218,22 +218,23 @@ def fitForecast(y, h, sumMat, nodes, method, freq, include_history, cap, capF, c
 
     return forecastsDict
 
-#%%
+
+# %%
 def forecastProp(forecastsDict, nodes):
     '''
      Cons:
        Produces biased revised forecasts even if base forecasts are unbiased
     '''
-    nCols = len(list(forecastsDict.keys()))+1
+    nCols = len(list(forecastsDict.keys())) + 1
     ##
     # Find proportions of forecast at each step ahead, and then alter forecasts
     ##
     levels = len(nodes)
     column = 0
     firstNode = 1
-    newMat = np.empty([len(forecastsDict[0].yhat),nCols - 1])
-    newMat[:,0] = forecastsDict[0].yhat
-    lst = [x for x in range(nCols-1)]
+    newMat = np.empty([len(forecastsDict[0].yhat), nCols - 1])
+    newMat[:, 0] = forecastsDict[0].yhat
+    lst = [x for x in range(nCols - 1)]
     for level in range(levels):
         nodesInLevel = len(nodes[level])
         foreSum = 0
@@ -242,46 +243,50 @@ def forecastProp(forecastsDict, nodes):
             lastNode = firstNode + numChild
             lst = [x for x in range(firstNode, lastNode)]
             baseFcst = np.array([forecastsDict[k].yhat[:] for k in lst])
-            foreSum = np.sum(baseFcst, axis = 0)
+            foreSum = np.sum(baseFcst, axis=0)
             foreSum = foreSum[:, np.newaxis]
             if column == 0:
                 revTop = np.array(forecastsDict[column].yhat)
                 revTop = revTop[:, np.newaxis]
             else:
-                revTop = np.array(newMat[:,column])
+                revTop = np.array(newMat[:, column])
                 revTop = revTop[:, np.newaxis]
-            newMat[:,firstNode:lastNode] = np.divide(np.multiply(np.transpose(baseFcst), revTop), foreSum)
+            newMat[:, firstNode:lastNode] = np.divide(np.multiply(np.transpose(baseFcst), revTop), foreSum)
             column += 1
             firstNode += numChild
 
     return newMat
 
-#%%
-def optimalComb(forecastsDict, sumMat, method, mse):
 
-    hatMat = np.zeros([len(forecastsDict[0].yhat),1])
+# %%
+def optimalComb(forecastsDict, sumMat, method, mse):
+    hatMat = np.zeros([len(forecastsDict[0].yhat), 1])
     for key in forecastsDict.keys():
         f1 = np.array(forecastsDict[key].yhat)
         f2 = f1[:, np.newaxis]
         if np.all(hatMat == 0):
             hatMat = f2
         else:
-            hatMat = np.concatenate((hatMat, f2), axis = 1)
+            hatMat = np.concatenate((hatMat, f2), axis=1)
     ##
     # Multiply the Summing Matrix Together S*inv(S'S)*S'
     ##
     if method == "OLS":
-        optiMat = np.dot(np.dot(sumMat, np.linalg.inv(np.dot(np.transpose(sumMat), sumMat))),np.transpose(sumMat))
+        optiMat = np.dot(np.dot(sumMat, np.linalg.inv(np.dot(np.transpose(sumMat), sumMat))), np.transpose(sumMat))
     if method == "WLSS":
-        diagMat = np.diag(np.transpose(np.sum(sumMat, axis = 1)))
-        optiMat = np.dot(np.dot(np.dot(sumMat, np.linalg.inv(np.dot(np.dot(np.transpose(sumMat), np.linalg.inv(diagMat)), sumMat))), np.transpose(sumMat)), np.linalg.inv(diagMat))
+        diagMat = np.diag(np.transpose(np.sum(sumMat, axis=1)))
+        optiMat = np.dot(
+            np.dot(np.dot(sumMat, np.linalg.inv(np.dot(np.dot(np.transpose(sumMat), np.linalg.inv(diagMat)), sumMat))),
+                   np.transpose(sumMat)), np.linalg.inv(diagMat))
     if method == "WLSV":
         diagMat = [mse[key] for key in mse.keys()]
-        diagMat = np.diag(np.flip(np.hstack(diagMat)+0.0000001, 0))
-        optiMat = np.dot(np.dot(np.dot(sumMat, np.linalg.inv(np.dot(np.dot(np.transpose(sumMat), np.linalg.inv(diagMat)), sumMat))), np.transpose(sumMat)), np.linalg.inv(diagMat))
+        diagMat = np.diag(np.flip(np.hstack(diagMat) + 0.0000001, 0))
+        optiMat = np.dot(
+            np.dot(np.dot(sumMat, np.linalg.inv(np.dot(np.dot(np.transpose(sumMat), np.linalg.inv(diagMat)), sumMat))),
+                   np.transpose(sumMat)), np.linalg.inv(diagMat))
 
-    newMat = np.empty([hatMat.shape[0],sumMat.shape[0]])
+    newMat = np.empty([hatMat.shape[0], sumMat.shape[0]])
     for i in range(hatMat.shape[0]):
-        newMat[i,:] = np.dot(optiMat, np.transpose(hatMat[i,:]))
+        newMat[i, :] = np.dot(optiMat, np.transpose(hatMat[i, :]))
 
     return newMat
