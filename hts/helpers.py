af1acf3496637c7b959993077d515f7ea174b970
carlomazzaferro <carlo.mazzaferro@gmail.com>
2019-11-26 18:06:24 +0100

    Helper functions

--- hts/helpers.py
+++ hts/helpers.py
@@ -1,55 +1,28 @@
-import re
-import os
-
-from nltk.corpus import stopwords
-from nltk.stem.wordnet import WordNetLemmatizer
-
-lem = WordNetLemmatizer()
-
-custom_stopwords = {'the', 'to', 'and', 'of', 'in', 'are', 'target', 'business', 'redemption', 'complete', 'initial',
-                    'sponsor', 'redeem', 'tender', 'consummate', 'offer', 'proposed', 'table', 'content', 'could',
-                    'quarter', 'forward', 'looking', 'http', 'www', 'businesswire', 'com', 'prnewswire', 'gaap', 'inc',
-                    'diluted', 'share', 'common', 'stock', 'tax', 'seek', 'ordinary', 'right', 'liability', 'llc',
-                    'third', 'party', 'holding', 'company', 'corporation', 'adversely', 'first', 'income', 'net',
-                    'nasdaq', 'statement', 'shareholder', 'stockholder', 'march', 'ended', 'end', 'non', 'solution',
-                    'ebitda', 'may', 'fund', 'trademark', 'sold', 'offering', 'dow', 'jones', 'barclays' 'barclay',
-                    'indxx', 'etf', 'etfs', 'index', 'trust', 'per', 'etns', 'co', 'ltd', 'china', 'prc', 'make', 'mr',
-                    'incorporation', 'certificate', 'incorporation', 'consummation', 'in', 'warrant',
-                    'combination', 'shall', 'item'
-                    }
+from h3 import h3
+import pandas
 
 
-def read_filter(p):
-    for path in os.listdir(p):
-        if path.endswith('.txt'):
-            with open(os.path.join(p, path), encoding='utf-8') as inf:
-                r = inf.read()
-                if len(r) == 0:
-                    continue
-                else:
-                    yield r, os.path.splitext(os.path.basename(path))[0]
+def hexify(df, lat_col, lon_col, levels=(6, 8)):
+    for r in range(levels[0], levels[1] + 1):
+        df[f'hex_index_{r}'] = df.apply(lambda x: h3.geo_to_h3(x[lat_col], x[lon_col], r), 1)
+    return df
 
 
-def sanitize_dict(d):
-    d = {k: v for k, v in d.items() if any([isinstance(v, str),
-                                            isinstance(v, float),
-                                            isinstance(v, int),
-                                            isinstance(v, bool),
-                                            isinstance(v, tuple)])}
-    for k, v in d.items():
-        if isinstance(v, tuple):
-            d[k] = list(v)
-    return d
+def resample_count(df, freq, colname):
+    df[colname] = df.index
+    _df = df[[colname]]
+    return _df.resample(freq).agg('count')
 
 
-def preprocess_text(doc):
-    st = stopwords.words('english')
-    text = re.sub('[^a-zA-Z]', ' ', doc)
-    text = text.lower()
-    text = re.sub("&lt;/?.*?&gt;", " &lt;&gt; ", text)
-    text = re.sub("(\\d|\\W)+", " ", text)
-    text = text.split()
-    text = [lem.lemmatize(word) for word in text if word not in st]
-    text = [t for t in text if t not in custom_stopwords]
-    text = " ".join(text)
-    return text
+def groupify(df, freq='1H', groups=None):
+    total = resample_count(df, freq, 'total')
+    children = [total]
+    for group in groups:
+        unique_in_group = df[group].unique()
+        for u in unique_in_group:
+            sub_df = df[df[group] == u]
+            if len(sub_df) < 1000:
+                continue
+            resampled = resample_count(sub_df, freq, '_'.join([group, u]))
+            children.append(resampled)
+    return pandas.concat(children, 1, sort=False)
