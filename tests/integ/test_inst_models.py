9d12b14f8d40ab91f7d45d27f82d1facaf3464e8
carlomazzaferro <carlo.mazzaferro@gmail.com>
2020-02-14 18:45:19 +0100

    auto arima tests

--- tests/integ/test_inst_models.py
+++ tests/integ/test_inst_models.py
@@ -1,8 +1,11 @@
 import numpy
 import pandas
 from fbprophet import Prophet
+from pmdarima import AutoARIMA
 
-from hts import FBProphetModel
+from hts.model.p import FBProphetModel
+from hts.model.ar import AutoArimaModel, SarimaxModel
+from hts.model.es import HoltWintersModel
 from hts.model import TimeSeriesModel
 
 
@@ -39,6 +42,18 @@ def test_fit_predict_fb_model_uv(uv_tree):
     assert isinstance(fb.forecast, pandas.DataFrame)
     assert isinstance(fb.residual, numpy.ndarray)
     assert isinstance(fb.mse, float)
+
+
+def test_fit_predict_ar_model_uv(uv_tree):
+    ar = AutoArimaModel(
+        node=uv_tree,
+        max_iter=2,
+    )
+    ar.fit_predict()
+    assert isinstance(ar.model, AutoARIMA)
+    assert isinstance(ar.forecast, numpy.ndarray)
+    assert isinstance(ar.residual, numpy.ndarray)
+    assert isinstance(ar.mse, float)
 
 
 
