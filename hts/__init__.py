65bfa5597736360dea73bde8248dc4c3068ffaee
carlomazzaferro <carlo.mazzaferro@gmail.com>
2020-03-28 14:01:55 +0100

    Bump version: 0.2.2 → 0.2.3

--- hts/__init__.py
+++ hts/__init__.py
@@ -3,7 +3,7 @@ from hts.revision import RevisionMethod
 
 __author__ = """Carlo Mazzaferro"""
 __email__ = 'carlo.mazzaferro@gmail.com'
-__version__ = '0.2.2'
+__version__ = '0.2.3'
 
 
 __all__ = [
