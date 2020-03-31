b8f5e1b3405cdcc734ca514ac0f62dbe1c87f7d6
carlomazzaferro <carlo.mazzaferro@gmail.com>
2020-03-31 16:46:26 +0200

    Tests for transforms, omit typing defs from test coverage

--- hts/transforms.py
+++ hts/transforms.py
@@ -1,5 +1,7 @@
 import logging
+from typing import Union
 
+import numpy
 import pandas
 from sklearn.base import BaseEstimator, TransformerMixin
 from scipy.special import inv_boxcox
@@ -19,35 +21,28 @@ class FunctionTransformer(BaseEstimator, TransformerMixin):
         self.inv_func = inv_func
         self.lam = None
 
-    def fit(self, X, y, **fit_params):
+    def fit(self, x: pandas.Series, y=None, **fit_params):
         return self
 
-    def transform(self, X):
-        try:
-            for col in X.columns[1:]:
-                X[col], self.lam = self.func(X[col])
-        except RuntimeWarning as e:
-            logger.error(f'Coulnd\'t perform transform: {repr(e)}')
-        return X
+    def transform(self, x: pandas.Series):
+        if any(x.values == 0):
+            x += 1
+            x, self.lam = self.func(x.values)
+            return x
+        elif any(x.values < 0):
+            raise ValueError('Boxcox can\t be applied, column has negative values')
+        else:
+            x, self.lam = self.func(x.values)
+            return x
 
-    def fit_transform(self, X, y=None, **fit_params):
-        try:
-            for col in X.columns[1:]:
-                X[col], self.lam = self.func(X[col], **fit_params)
-        except RuntimeWarning as e:
-            logger.error(f'Coulnd\'t perform transform: {repr(e)}')
-        return X
+    def fit_transform(self, x: pandas.Series, y=None, **fit_params):
+        return self.fit(x).transform(x)
 
-    def inverse_transform(self, X):
-        if isinstance(X, pandas.Series):
-            return self.inv_func(X, self.lam)
-        try:
-            for col in X.columns[1:]:
-                X[col] = self.inv_func(X[col], self.lam)
-        except RuntimeWarning as e:
-            logger.error(f'Coulnd\'t perform transform: {repr(e)}')
-        return X
-
+    def inverse_transform(self, x: Union[pandas.Series, numpy.ndarray]):
+        if isinstance(x, pandas.Series):
+            return self.inv_func(x.values, self.lam)
+        else:
+            return self.inv_func(x, self.lam)
 
 
 
