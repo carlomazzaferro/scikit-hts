28e38e2f29c5c71669303f4df39adab126237acb
javierhuertay <javier@Rivian.localdomain>
2021-04-16 20:11:58 -0600

    fixing black and isort format

--- hts/revision.py
+++ hts/revision.py
@@ -31,7 +31,9 @@ class RevisionMethod(object):
     def _y_hat_matrix(self, forecasts) -> numpy.ndarray:
         n_cols = len(list(forecasts.keys())) + 1
         if self.name == "BU":
-            keys = list(forecasts.keys())[n_cols - self.sum_mat.shape[1] - 1 : n_cols - 1]
+            keys = list(forecasts.keys())[
+                n_cols - self.sum_mat.shape[1] - 1 : n_cols - 1
+            ]
         else:
             keys = range(n_cols - self.sum_mat.shape[1] - 1, n_cols - 1)
         return y_hat_matrix(forecasts, keys=keys)
@@ -54,7 +56,9 @@ class RevisionMethod(object):
             return y_hat_matrix(forecasts=forecasts)
 
         if self.name in [MethodT.OLS.name, MethodT.WLSS.name, MethodT.WLSV.name]:
-            return optimal_combination(forecasts=forecasts, sum_mat=self.sum_mat, method=self.name, mse=mse)
+            return optimal_combination(
+                forecasts=forecasts, sum_mat=self.sum_mat, method=self.name, mse=mse
+            )
 
         elif self.name == MethodT.BU.name:
             y_hat = self._y_hat_matrix(forecasts)
@@ -63,8 +67,12 @@ class RevisionMethod(object):
         elif self.name in [MethodT.AHP.name, MethodT.PHA.name]:
             if self.transformer:
                 for node in make_iterable(nodes, prop=None):
-                    node.item[node.key] = self.transformer.inverse_transform(node.item[node.key])
-            y_hat = proportions(nodes=nodes, forecasts=forecasts, sum_mat=self.sum_mat, method=self.name)
+                    node.item[node.key] = self.transformer.inverse_transform(
+                        node.item[node.key]
+                    )
+            y_hat = proportions(
+                nodes=nodes, forecasts=forecasts, sum_mat=self.sum_mat, method=self.name
+            )
             return self._new_mat(y_hat)
 
         elif self.name == MethodT.FP.name:
